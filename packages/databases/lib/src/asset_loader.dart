3ff720d3fc003eb094b3334ee32246c545e0b6c1
yudystriawan <yudistriawan@gmail.com>
2025-02-27 22:53:24 +0800

    Update load method in LocalAssetLoader to remove static modifier

--- packages/databases/lib/src/asset_loader.dart
+++ packages/databases/lib/src/asset_loader.dart
@@ -1,7 +1,7 @@
 part of '../databases.dart';
 
 class LocalAssetLoader {
-  static Future<Map<dynamic, String>> load(String path) async {
+  Future<Map<dynamic, String>> load(String path) async {
     final jsonString = await rootBundle.loadString('assets/json/$path');
     final data = jsonDecode(jsonString);
     return data;
