ca6bff3f5d1ae3e1c2f0b0d3f79ce99199815965
yudystriawan <yudistriawan@gmail.com>
2025-02-28 07:38:05 +0800

    Add UserProfileWidget to display user details and integrate AppChip for description

---
+++ features/users/lib/presentation/widgets/user_profile_widget.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ui/ui.dart';
+import 'package:users/presentation/blocs/bloc/user_loader_bloc.dart';
+import 'package:users/users.dart';
+
+class UserProfileWidget extends StatelessWidget {
+  const UserProfileWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<UserLoaderBloc, UserLoaderState>(
+      builder: (context, state) {
+        return switch (state) {
+          Initial() => SizedBox(),
+          LoadInProgress() => CircularProgressIndicator(),
+          LoadFailure() => Column(
+            children: [
+              Text('Failed to load user'),
+              IconButton(
+                onPressed: () => context.read<UserLoaderBloc>(),
+                icon: Icon(Icons.refresh),
+              ),
+            ],
+          ),
+          LoadSuccess() => Column(
+            children: [
+              Container(
+                constraints: BoxConstraints(minWidth: 160.w),
+                color: Colors.white,
+                child: Column(
+                  children: [
+                    Container(
+                      width: 120.w,
+                      height: 120.w,
+
+                      decoration: BoxDecoration(shape: BoxShape.circle),
+                      child: UserProfileImage(url: state.user.imageUrl),
+                    ),
+                    SizedBox(height: 12.w),
+                    Text(state.user.name, style: TextStyle(fontSize: 20.sp)),
+                    SizedBox(height: 2.w),
+                    Text(
+                      state.user.tier.toString(),
+                      style: TextStyle(
+                        fontSize: 14.sp,
+                        color: Color(0xffFFD233),
+                      ),
+                    ),
+                    SizedBox(height: 18.w),
+                    AppChip(label: state.user.description),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        };
+      },
+    );
+  }
+}
