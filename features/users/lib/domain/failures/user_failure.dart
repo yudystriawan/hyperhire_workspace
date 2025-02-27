052902a0f725f64d9c831e0d861114c49640a93b
yudystriawan <yudistriawan@gmail.com>
2025-02-27 22:22:35 +0800

    Add UserFailure class and export failures in domain layer

---
+++ features/users/lib/domain/failures/user_failure.dart
@@ -0,0 +1,8 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user_failure.freezed.dart';
+
+@freezed
+sealed class UserFailure with _$UserFailure {
+  const factory UserFailure.unexpectedError() = UnexpectedError;
+}
