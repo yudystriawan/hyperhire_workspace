8bf3c9c11bd071f0b439084597f992756d5fc99b
yudystriawan <yudistriawan@gmail.com>
2025-02-28 06:26:47 +0800

    Implement user data retrieval from local JSON file in UserDatasource

--- features/users/lib/data/datasources/user_datasource.dart
+++ features/users/lib/data/datasources/user_datasource.dart
@@ -1,3 +1,4 @@
+import 'package:databases/databases.dart';
 import 'package:injectable/injectable.dart';
 import 'package:users/users.dart';
 
@@ -7,9 +8,16 @@ abstract class UserDatasource {
 
 @Injectable(as: UserDatasource)
 class UserDatasourceImpl implements UserDatasource {
+  final LocalAssetLoader db;
+
+  UserDatasourceImpl(this.db);
+
   @override
-  Future<UserDto?> getUser(String id) {
-    // TODO: implement getUser
-    throw UnimplementedError();
+  Future<UserDto?> getUser(String id) async {
+    final data = await db.load('users.json');
+    final users =
+        (data['data'] as List).map((e) => UserDto.fromJson(e)).toList();
+    final user = users.firstWhere((element) => element.id == id);
+    return user;
   }
 }
