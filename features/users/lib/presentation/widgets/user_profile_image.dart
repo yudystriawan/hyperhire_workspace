f04abaedd2e5f4e89abfc2eaf57ac0701dbc77f6
yudystriawan <yudistriawan@gmail.com>
2025-02-27 23:52:18 +0800

    Add user profile images and create UserProfileImage widget

---
+++ features/users/lib/presentation/widgets/user_profile_image.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class UserProfileImage extends StatelessWidget {
+  const UserProfileImage({
+    super.key,
+    required this.url,
+    this.fit,
+    this.width,
+    this.height,
+  });
+
+  final String url;
+  final BoxFit? fit;
+  final double? width;
+  final double? height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image(
+      image: AssetImage('assets/images/$url', package: 'products'),
+      fit: fit,
+      height: height,
+      width: width,
+    );
+  }
+}
