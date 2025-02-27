acac1a93a6808f311d7eca38e2776ed6a00fce28
yudystriawan <yudistriawan@gmail.com>
2025-02-28 06:51:09 +0800

    Refactor UserLoaderBloc to use GetUserDetailUsecase and handle fetched events

--- features/users/lib/presentation/blocs/bloc/user_loader_bloc.dart
+++ features/users/lib/presentation/blocs/bloc/user_loader_bloc.dart
@@ -1,15 +1,29 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
 import 'package:users/users.dart';
 
 part 'user_loader_bloc.freezed.dart';
 part 'user_loader_event.dart';
 part 'user_loader_state.dart';
 
+@injectable
 class UserLoaderBloc extends Bloc<UserLoaderEvent, UserLoaderState> {
-  UserLoaderBloc() : super(Initial()) {
-    on<UserLoaderEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final GetUserDetailUsecase _getUserDetailUsecase;
+  UserLoaderBloc(this._getUserDetailUsecase) : super(Initial()) {
+    on<_Fetched>(_onFetched);
+  }
+
+  Future<void> _onFetched(_Fetched event, Emitter<UserLoaderState> emit) async {
+    if (state is LoadInProgress) return;
+
+    emit(LoadInProgress());
+    final result = await _getUserDetailUsecase(event.id);
+    emit(
+      result.fold(
+        (failure) => LoadFailure(failure),
+        (user) => LoadSuccess(user),
+      ),
+    );
   }
 }
