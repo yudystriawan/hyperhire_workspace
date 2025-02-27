9ffe0e8ec1f4bda5c3a8cb89a9ccf26b5b3e56fe
yudystriawan <yudistriawan@gmail.com>
2025-02-27 09:57:46 +0800

    Refactor Usecase to include parameters for improved flexibility

--- packages/core/lib/usecases/usecase.dart
+++ packages/core/lib/usecases/usecase.dart
@@ -1,5 +1,9 @@
 part of '../core.dart';
 
-abstract class Usecase<L, R> {
+abstract class Usecase<L, R, Params> {
+  Future<Either<L, R>> call(Params id);
+}
+
+abstract class NoParamsUsecase<L, R> {
   Future<Either<L, R>> call();
 }
