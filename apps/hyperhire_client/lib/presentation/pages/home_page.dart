67a1b620a27fb108da82ffc25c9f272a739d9e62
yudystriawan <yudistriawan@gmail.com>
2025-02-27 14:22:01 +0800

    Add ProductImageWidget to HomePage and wrap body in SingleChildScrollView

--- apps/hyperhire_client/lib/presentation/pages/home_page.dart
+++ apps/hyperhire_client/lib/presentation/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:banners/banners.dart';
 import 'package:flutter/material.dart';
+import 'package:products/presentation/widgets/product_image_widget.dart';
 
 @RoutePage()
 class HomePage extends StatelessWidget {
@@ -10,7 +11,14 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('LOGO')),
-      body: BannerCarousel(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            BannerCarousel(),
+            ProductImageWidget(url: 'img_product_1.png'),
+          ],
+        ),
+      ),
     );
   }
 }
