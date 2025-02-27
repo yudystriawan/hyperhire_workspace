b7cbe8d4d1b5ecaa70d640ae0371a48520c2d115
yudystriawan <yudistriawan@gmail.com>
2025-02-28 07:37:11 +0800

    Add AppChip widget and update exports in ui.dart and widgets.dart

--- packages/ui/lib/ui.dart
+++ packages/ui/lib/ui.dart
@@ -1,2 +1,4 @@
 export 'package:flutter_screenutil/flutter_screenutil.dart';
 export 'package:google_fonts/google_fonts.dart';
+
+export 'widgets/widgets.dart';
---
+++ packages/ui/lib/widgets/chip.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppChip extends StatelessWidget {
+  const AppChip({super.key, required this.label});
+
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(6.r),
+        color: Color(0xfff0f0f0),
+      ),
+      padding: EdgeInsets.symmetric(vertical: 5.w, horizontal: 6.w),
+      child: Center(
+        child: Text(
+          label,
+          style: TextStyle(fontSize: 11.sp, color: Color(0xff868686)),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
