9008d866e62d79386e7378b3d917eaca1877d43a
yudystriawan <yudistriawan@gmail.com>
2025-02-27 10:39:39 +0800

    Add UserMapper class for mapping between User and UserDto, and update pubspec.yaml for injectable dependency

--- features/users/lib/data/data.dart
+++ features/users/lib/data/data.dart
@@ -1,1 +1,2 @@
+export 'mapper/mapper.dart';
 export 'models/models.dart';
---
+++ features/users/lib/data/mapper/user_mapper.dart
@@ -0,0 +1,38 @@
+import 'package:core/core.dart';
+import 'package:injectable/injectable.dart';
+import 'package:users/users.dart';
+
+@injectable
+class UserMapper implements Mapper<User, UserDto> {
+  @override
+  User mapToEntity(UserDto model) {
+    return User(
+      id: model.id,
+      imageUrl: model.imageUrl,
+      name: model.name,
+      tier: switch (model.tier) {
+        'bronze' => UserTier.bronze(),
+        'silver' => UserTier.silver(),
+        'gold' => UserTier.gold(),
+        _ => throw Exception('Invalid tier'),
+      },
+      description: model.description,
+    );
+  }
+
+  @override
+  UserDto mapToModel(User entity) {
+    return UserDto(
+      id: entity.id,
+      imageUrl: entity.imageUrl,
+      name: entity.name,
+      tier: switch (entity.tier) {
+        Bronze() => 'bronze',
+        Silver() => 'silver',
+        Gold() => 'gold',
+        _ => throw Exception('Invalid tier'),
+      },
+      description: entity.description,
+    );
+  }
+}
