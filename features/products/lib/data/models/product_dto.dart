94e19a6c685aed98ec7a7bdd4616e42e09880f71
yudystriawan <yudistriawan@gmail.com>
2025-02-27 10:02:07 +0800

    Add ProductDto model and update exports for data layer

---
+++ features/products/lib/data/models/product_dto.dart
@@ -0,0 +1,16 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'product_dto.freezed.dart';
+part 'product_dto.g.dart';
+
+@freezed
+abstract class ProductDto with _$ProductDto {
+  const factory ProductDto({
+    required String id,
+    required String imageUrl,
+    required String title,
+  }) = _ProductDto;
+
+  factory ProductDto.fromJson(Map<String, dynamic> json) =>
+      _$ProductDtoFromJson(json);
+}
