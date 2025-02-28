4ff85e997a7e4d3d286f2f7a61cabc09210fff43
yudystriawan <yudistriawan@gmail.com>
2025-02-28 08:25:13 +0800

    Update LocalDBModule to use lazy singleton for LocalAssetLoader

--- features/reviews/lib/di/modules/localdb_module.dart
+++ features/reviews/lib/di/modules/localdb_module.dart
@@ -3,5 +3,6 @@ import 'package:injectable/injectable.dart';
 
 @module
 abstract class LocalDBModule {
-  LocalAssetLoader get localAssetLoader => LocalAssetLoader();
+  @lazySingleton
+  LocalAssetLoader get reviewDb => LocalAssetLoader();
 }
