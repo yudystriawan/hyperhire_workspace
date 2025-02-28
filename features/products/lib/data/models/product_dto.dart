af63b7aa056768cbb41830fe0ecf19dac06b2d1a
yudystriawan <yudistriawan@gmail.com>
2025-02-28 16:03:33 +0800

    Remove unused imports and update dependencies in product DTO and database package

--- features/products/lib/data/models/product_dto.dart
+++ features/products/lib/data/models/product_dto.dart
@@ -1,10 +1,8 @@
-import 'package:databases/databases.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'product_dto.freezed.dart';
 part 'product_dto.g.dart';
 
-@Collection(ignore: {'copyWith'})
 @freezed
 abstract class ProductDto with _$ProductDto {
   const factory ProductDto({
--- packages/databases/lib/databases.dart
+++ packages/databases/lib/databases.dart
@@ -2,8 +2,6 @@ import 'dart:convert';
 
 import 'package:flutter/services.dart';
 
-export 'package:isar/isar.dart';
-
 export 'di/di.dart';
 
 part 'src/asset_loader.dart';
