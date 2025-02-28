894cc2cbf3373a4f54adcfb684aa83b1afd53d7c
yudystriawan <yudistriawan@gmail.com>
2025-02-28 08:21:09 +0800

    Add UsersPackageModule to dependency injection configuration

--- apps/hyperhire_client/lib/di/di.dart
+++ apps/hyperhire_client/lib/di/di.dart
@@ -3,11 +3,13 @@ import 'package:core/core.dart';
 import 'package:hyperhire_client/di/di.config.dart';
 import 'package:injectable/injectable.dart';
 import 'package:reviews/reviews.dart';
+import 'package:users/users.dart';
 
 @InjectableInit(
   externalPackageModulesAfter: [
     ExternalModule(BannersPackageModule),
     ExternalModule(ReviewsPackageModule),
+    ExternalModule(UsersPackageModule),
   ],
 )
 Future<void> configureDependencies() => getIt.init();
