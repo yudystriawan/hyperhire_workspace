832c7dce73d4784ba207ea4d36c852decf5bb2f5
yudystriawan <yudistriawan@gmail.com>
2025-02-28 16:18:53 +0800

    Refactor ReviewerListItemWidget to enhance layout with a stack and add crown icon for top reviewers

--- features/reviews/lib/presentation/widgets/reviewer_list/reviewer_list_item_widget.dart
+++ features/reviews/lib/presentation/widgets/reviewer_list/reviewer_list_item_widget.dart
@@ -19,27 +19,46 @@ class ReviewerListItemWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap != null ? () => onTap!(reviewer.user.id) : null,
-      child: SizedBox(
-        width: 62.w,
-        height: 106.w,
-        child: Column(
-          children: [
-            Container(
-              width: 62.w,
-              height: 62.w,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(color: Colors.transparent, width: 1.w),
+      child: Stack(
+        children: [
+          SizedBox(
+            width: 62.w,
+            height: 106.w,
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Container(
+                    width: 62.w,
+                    height: 62.w,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color:
+                            topReviewer
+                                ? Color(0xffFFD233)
+                                : Colors.transparent,
+                        width: topReviewer ? 4.w : 1.w,
+                      ),
+                    ),
+                    child: UserProfileImage(url: reviewer.user.profileImageUrl),
+                  ),
+                  SizedBox(height: 10.w),
+                  Text(
+                    reviewer.user.name,
+                    style: TextStyle(fontSize: 14.sp, color: Color(0xff616161)),
+                  ),
+                ],
               ),
-              child: UserProfileImage(url: reviewer.user.profileImageUrl),
             ),
-            SizedBox(height: 10.w),
-            Text(
-              reviewer.user.name,
-              style: TextStyle(fontSize: 14.sp, color: Color(0xff616161)),
+          ),
+          if (topReviewer)
+            Positioned(
+              top: 0,
+              left: 0,
+              child: AppIcon(AppIcons.icCrownGold, size: 15.w),
             ),
-          ],
-        ),
+        ],
       ),
     );
   }
