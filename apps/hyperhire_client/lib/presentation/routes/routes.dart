408427da7dd0ab532a8299ea44ae8629925c2fbb
yudystriawan <yudistriawan@gmail.com>
2025-02-28 08:19:13 +0800

    Add UserDetailRoute to app routing

--- apps/hyperhire_client/lib/presentation/routes/routes.dart
+++ apps/hyperhire_client/lib/presentation/routes/routes.dart
@@ -6,5 +6,6 @@ class AppRouter extends RootStackRouter {
   @override
   List<AutoRoute> get routes => [
     AutoRoute(page: HomeRoute.page, initial: true),
+    AutoRoute(page: UserDetailRoute.page),
   ];
 }
